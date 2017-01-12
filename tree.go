82ea53cdffa05f5a70dcec6517ba8c538a0aeed0
Lee R. Boynton <lee@boynton.com>
2017-01-12 11:34:22 -0800

    fixed a few bugs.

--- tree.go
+++ tree.go
@@ -18,72 +18,83 @@ func main() {
 	//-I exclude matches to pattern
 	flag.Parse()
 	args := flag.Args()
-	var fileFilter func(os.FileInfo)bool
+	var fileFilter func(os.FileInfo) bool
 	if !*all {
 		fileFilter = defaultFilter
 	}
-	filter := func(files []os.FileInfo) []os.FileInfo {
-		if fileFilter == nil {
-			return files
-		}
-		result := make([]os.FileInfo, 0)
-		for _, info := range files {
-			if !fileFilter(info) {
-				result = append(result, info)
-			}
-		}
-		return result
-	}
-
 	if len(args) > 0 {
 		for _, dir := range args {
-			tree(dir, filter, *ftype)
+			tree(dir, fileFilter, *ftype)
 		}
 	} else {
-		tree(".", filter, *ftype)
+		tree(".", fileFilter, *ftype)
 	}
 }
 
-func tree(dir string, filter func([]os.FileInfo) []os.FileInfo, ftype bool) {
-	fmt.Printf("%s\n", filepath.Base(dir))
+func tree(dir string, filter func(os.FileInfo) bool, ftype bool) {
 	subtree(dir, filter, ftype, "")
 }
 
 func defaultFilter(info os.FileInfo) bool {
 	name := info.Name()
-	if strings.HasPrefix(name, ".") || strings.HasSuffix(name, "~") {
+	if strings.HasPrefix(name, ".") || strings.HasSuffix(name, "~") || strings.HasPrefix(name, "#") {
 		return true
 	}
 	return false
 }
 
-func subtree(dir string, filter func([]os.FileInfo) []os.FileInfo, ftype bool, indent string) {
+type Info struct {
+	Name  string
+	IsDir bool
+	Mode  os.FileMode
+}
+
+func readDir(dir string, filter func(os.FileInfo) bool) ([]Info, error) {
 	infos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]Info, 0, len(infos))
+	for _, info := range infos {
+		if filter == nil || !filter(info) {
+			result = append(result, Info{Name: info.Name(), IsDir: info.IsDir(), Mode: info.Mode()})
+		}
+	}
+	return result, nil
+}
+
+func subtree(dir string, filter func(os.FileInfo) bool, ftype bool, indent string) {
+	infos, err := readDir(dir, filter)
 	if err == nil {
-		infos = filter(infos)
-		i := 0
 		last := len(infos) - 1
 		spacer := "├──"
-		for _, info := range infos {
-			name := info.Name()
+		max := len(infos)
+		for i := 0; i < max; i++ {
+			info := infos[i]
+			name := info.Name
 			if i == last {
 				spacer = "└──"
 			}
-			i++
 			if ftype {
 				fmt.Printf("%s%s %s%s\n", indent, spacer, name, fileTypeSuffix(info))
 			} else {
 				fmt.Printf("%s%s %s\n", indent, spacer, name)
 			}
-			if info.IsDir() {
-				subtree(filepath.Join(dir, name), filter, ftype, indent + "    ")
+			if info.IsDir {
+				var nextIndent string
+				if i+1 < max {
+					nextIndent = indent + "│   "
+				} else {
+					nextIndent = indent + "    "
+				}
+				subtree(filepath.Join(dir, name), filter, ftype, nextIndent)
 			}
 		}
 	}
 }
 
-func fileTypeSuffix(info os.FileInfo) string {
-	switch info.Mode() & os.ModeType {
+func fileTypeSuffix(info Info) string {
+	switch info.Mode & os.ModeType {
 	case os.ModeDir:
 		return "/"
 	case os.ModeSymlink:
@@ -93,7 +104,7 @@ func fileTypeSuffix(info os.FileInfo) string {
 	case os.ModeNamedPipe:
 		return "|"
 	}
-	perms := info.Mode() & os.ModePerm
+	perms := info.Mode & os.ModePerm
 	if (perms & 0111) != 0 {
 		return "*"
 	}
